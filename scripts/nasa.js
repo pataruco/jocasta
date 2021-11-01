5c7235079dd70137dbdc9262927604ce658711a9
Pedro Martin <pedro.martin@hopin.to>
2021-11-01 14:48:55 +0000

    Draft nasa script

--- scripts/nasa.js
+++ scripts/nasa.js
@@ -1,21 +1,14 @@
 const request = require('request');
 
-Date.prototype.yyyymmdd = function () {
-  var mm = this.getMonth() + 1;
-  var dd = this.getDate();
+const yesterdayDate = new Date(new Date().setDate(new Date().getDate() - 1));
 
-  return [this.getFullYear(), !mm[1] && '-0', mm, dd[1] && '0', '-' + dd].join(
-    '',
-  );
-};
-
-const yesterday = new Date(new Date().setDate(new Date().getDate() - 1));
+const yesterday = yesterdayDate.toISOString().split('T')[0];
 
 const options = {
   method: 'GET',
   url: 'https://api.nasa.gov/planetary/apod',
   qs: {
-    date: `${yesterday.yyyymmdd()}`,
+    date: `${yesterday}`,
     hd: 'True',
     api_key: '2QpdglwknKEulDdCV5ozKtmsme1fkk8d9gWb7Lnd',
   },
@@ -24,9 +17,9 @@ const options = {
 module.exports = (robot) => {
   return robot.hear(/nasa/i, (msg) => {
     options.qs.q = msg.match[1];
-    request(options, (error, response, body) => {
+    request(options, (error, response) => {
       if (error) throw new Error(error);
-      let nasa = JSON.parse(body);
+      let nasa = JSON.parse(response.body);
       let message = `*Title:* ${nasa.title}\n*Description:* ${nasa.explanation}\n${nasa.url}`;
       return msg.send(message);
     });

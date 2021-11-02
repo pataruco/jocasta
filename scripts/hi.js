b8f5fe89a411d4ecb2ba36bf973150430df02d28
Pedro Martin <pedro.martin@hopin.to>
2021-11-02 20:42:53 +0000

    Add doc

--- scripts/hi.js
+++ scripts/hi.js
@@ -1,3 +1,14 @@
+// Description:
+//
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//
 module.exports = (robot) => {
   return robot.hear(/(hi|hola)/gim, (msg) => {
     return msg.send('👋');
